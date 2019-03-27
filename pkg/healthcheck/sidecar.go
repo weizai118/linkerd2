5a165b2a73f7114b4c56ae4577e465a6f7811c93
Ivan Sim <ivan@buoyant.io>
2019-03-27 14:22:15 -0700

    Remove the check for the presence of a linkerd proxy in the input YAML
    This ensures that the proxy injector won't skip any existing CLI-injected
    workloads and be able to honor any newly added config override annotations
    
    Signed-off-by: Ivan Sim <ivan@buoyant.io>

--- pkg/healthcheck/sidecar.go
+++ pkg/healthcheck/sidecar.go
@@ -7,29 +7,42 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// HasExistingSidecars returns true if the pod spec already has the proxy init
-// and sidecar containers injected. Otherwise, it returns false.
-func HasExistingSidecars(podSpec *corev1.PodSpec) bool {
+const (
+	containerImageIstio       = "gcr.io/istio-release/proxyv2:"
+	containerImageContour     = "gcr.io/heptio-images/contour:"
+	containerImageEnvoy       = "docker.io/envoyproxy/envoy-alpine:"
+	containerNameIstio        = "istio-proxy"
+	containerNameContour      = "contour"
+	containerNameEnvoy        = "envoy"
+	initContainerImageIstio   = "gcr.io/istio-release/proxy_init:"
+	initContainerImageContour = "gcr.io/heptio-images/contour:"
+	initContainerNameIstio    = "istio-init"
+	initContainerNameContour  = "envoy-initconfig"
+)
+
+// HasOtherProxies returns true if the pod spec already has either an istio,
+// contour or envoy proxy container. Otherwise, it returns false.
+func HasOtherProxies(podSpec *corev1.PodSpec) bool {
 	for _, container := range podSpec.Containers {
 		if strings.HasPrefix(container.Image, "gcr.io/linkerd-io/proxy:") ||
-			strings.HasPrefix(container.Image, "gcr.io/istio-release/proxyv2:") ||
-			strings.HasPrefix(container.Image, "gcr.io/heptio-images/contour:") ||
-			strings.HasPrefix(container.Image, "docker.io/envoyproxy/envoy-alpine:") ||
+			strings.HasPrefix(container.Image, containerImageIstio) ||
+			strings.HasPrefix(container.Image, containerImageContour) ||
+			strings.HasPrefix(container.Image, containerImageEnvoy) ||
 			container.Name == k8s.ProxyContainerName ||
-			container.Name == "istio-proxy" ||
-			container.Name == "contour" ||
-			container.Name == "envoy" {
+			container.Name == containerNameIstio ||
+			container.Name == containerNameContour ||
+			container.Name == containerNameEnvoy {
 			return true
 		}
 	}
 
 	for _, ic := range podSpec.InitContainers {
 		if strings.HasPrefix(ic.Image, "gcr.io/linkerd-io/proxy-init:") ||
-			strings.HasPrefix(ic.Image, "gcr.io/istio-release/proxy_init:") ||
-			strings.HasPrefix(ic.Image, "gcr.io/heptio-images/contour:") ||
+			strings.HasPrefix(ic.Image, initContainerImageIstio) ||
+			strings.HasPrefix(ic.Image, initContainerImageContour) ||
 			ic.Name == "linkerd-init" ||
-			ic.Name == "istio-init" ||
-			ic.Name == "envoy-initconfig" {
+			ic.Name == initContainerNameIstio ||
+			ic.Name == initContainerNameContour {
 			return true
 		}
 	}
